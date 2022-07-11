36b24f8e5d740d0a00588572bc40029ec8584a67
sefa43 <jow090613@gmail.com>
2022-07-10 21:51:14 -0700

    finishing touches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WebcamModule } from 'ngx-webcam';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    
     NavbarComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     WebcamModule,
     BrowserAnimationsModule, 
     MatToolbarModule, 
@@ -28,3 +43,4 @@ import {MatIconModule} from '@angular/material/icon';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export const routingComponents = [AboutComponent, ContactComponent]
