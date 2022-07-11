ef612585c216f2517989f56c4d725aaa637d2ba0
sefa43 <jow090613@gmail.com>
2022-07-10 18:24:32 -0700

    CamLibrary
    CamLibrary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-CameraLibrary';
+
+
+  stream: any = null;
+  status: any = null;
+  trigger: Subject<void> = new Subject();
+  previewImage: string = '';
+  btnLabel: string = 'Capture image';
+
+  get $trigger(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  snapshot(event: WebcamImage) {
+    console.log(event);
+    this.previewImage = event.imageAsDataUrl;
+    this.btnLabel = 'Re-Capture Image'
+  }
+  checkPermissions() {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        width:500,
+        height:500
+      }
+    }).then((res) => {
+      console.log("response", res);
+      this.stream = res;
+      this.status = "My camera is connected"
+      this.btnLabel = 'Capture image';
+    }).catch(err =>{
+      console.log(err);
+      if(err?.message === 'Permission denied') {
+        this.status = 'Permission denied please try again'
+      } else {
+        this.status = 'No camera detected, please try again'
+      }
+    })
+  }
+
+
+  captureImage() {
+    this.trigger.next();
+  }
+
+  proceed(){
+    console.log(this.previewImage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WebcamModule } from 'ngx-webcam';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
